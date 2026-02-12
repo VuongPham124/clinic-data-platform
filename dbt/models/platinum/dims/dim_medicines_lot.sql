7c34c05234da94926ad14c4ed58e81d150a1bf02
VuLeHong <144672834+VuLeHong@users.noreply.github.com>
2026-02-12 14:03:40 +0700

    fix/ dim tables

--- dbt/models/platinum/dims/dim_medicines_lot.sql
+++ dbt/models/platinum/dims/dim_medicines_lot.sql
@@ -1,25 +1,24 @@
-{{ config(materialized='table') }}
-
 with src as (
   select
     cast(id as int64) as medicine_import_detail_id,
     cast(lot_number as string) as lot_number,
-    safe_cast(expire_date as timestamp) as expire_date,
+    safe_cast(manufacturing_date as date) as manufacturing_date,
+    safe_cast(expire_date as date) as expire_date,
     cast(import_price as numeric) as import_price,
-    cast(`status` as string) as lot_status,
+    cast(`status` as string) as lot_status
   from {{ source('silver', 'medicine_import_details') }}
-),
-dedup as (
-  select distinct medicine_import_detail_id
-  from src
-  where medicine_import_detail_id is not null
+  where id is not null
 )
 
 select
   abs(farm_fingerprint(cast(medicine_import_detail_id as string))) as lot_key,
   medicine_import_detail_id,
   lot_number,
+  manufacturing_date,
   expire_date,
   import_price,
-  lot_status
-from dedup
+  lot_status as status
+from (
+  select distinct *
+  from src
+)
