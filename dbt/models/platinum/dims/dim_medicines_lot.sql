92c511a93f36f97a9ec9666dd427c18f7fa311ed
VuLeHong <144672834+VuLeHong@users.noreply.github.com>
2026-02-24 10:44:55 +0700

    fix/adjust date value

--- dbt/models/platinum/dims/dim_medicines_lot.sql
+++ dbt/models/platinum/dims/dim_medicines_lot.sql
@@ -4,8 +4,8 @@ with src as (
   select
     cast(id as int64) as medicine_import_detail_id,
     cast(lot_number as string) as lot_number,
-    safe_cast(manufacturing_date as date) as manufacturing_date,
-    safe_cast(expire_date as date) as expire_date,
+    date(manufacturing_date) as manufacturing_date,
+    date(expire_date) as expire_date,
     cast(import_price as numeric) as import_price,
     cast(`status` as string) as lot_status
   from {{ source('silver', 'medicine_import_details') }}
